59a110428a050471be276aab2612b21031ef46ec
Suraj <surajrblue@gmail.com>
2019-10-02 12:00:29 -0500

    10/2/19-Suraj

---
+++ app/src/main/java/com/example/lifeactivityapp/MoviesChoicesActivity.java
@@ -0,0 +1,14 @@
+package com.example.lifeactivityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoviesChoicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_choices);
+    }
+}
