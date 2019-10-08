fee981061fbb5708d1ca4e35a99296b91fa1395c
xujessica <xujessica02@gmail.com>
2019-10-08 12:25:19 -0500

    Jess added restaurants class

---
+++ app/src/main/java/com/example/lifeactivityapp/Restaurants.java
@@ -0,0 +1,24 @@
+package com.example.lifeactivityapp;
+
+public class Restaurants {
+    private String restaurant;
+    private String description;
+    private int price;
+    //private int imageResourceID;
+
+    // price 0 = low and 1 = high
+    public Restaurants(String restaurant, String description,
+                int price) { //int imageResourceID) {
+        this.restaurant = restaurant;
+        this.description = description;
+        this.price = price;
+        //this.imageResourceID = imageResourceID;
+    }
+
+    public static final Restaurants[] breakfastPlaces = {
+            new Restaurants("Einstein Bros. Bagels", "", 0),
+            new Restaurants("Dunkin Donuts", "",0),
+            new Restaurants("Mcdonalds", "",0),
+            new Restaurants("Wildberry Pancakes & Cafe", "", 1)
+    };
+}
