25a613351d66acf53674c2b42fdf825adcbf8022
Jake Lee <jaketl2002@gmail.com>
2019-10-17 11:36:19 -0500

    entertainment choices layout

---
+++ app/src/main/java/com/example/lifeactivityapp/BreakfastSpinner.java
@@ -0,0 +1,14 @@
+package com.example.lifeactivityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BreakfastSpinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breakfast_spinner);
+    }
+}
