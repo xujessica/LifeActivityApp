57d5d635864c12dbcf3836b1b68fca219bd85320
xujessica <xujessica02@gmail.com>
2019-10-18 12:00:02 -0500

    Jess working on displaying filtered choice

--- app/src/main/java/com/example/lifeactivityapp/BreakfastFilterDisplayActivity.java
+++ app/src/main/java/com/example/lifeactivityapp/BreakfastFilterDisplayActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BreakfastFilterDisplayActivity extends AppCompatActivity {
 
@@ -19,24 +20,60 @@ public class BreakfastFilterDisplayActivity extends AppCompatActivity {
 
 
         //test
-        { Restaurants myRestaurants = Restaurants.breakfastPlaces[0];
+//
+
+//        { Restaurants breakfast = Restaurants.breakfastPlaces[0];
+//
+//        restaurant.setText(myRestaurants.getRestaurant());
+//        description.setText(myRestaurants.getDescription()); }
 
         TextView restaurant = (TextView) findViewById(R.id.restaurant);
         TextView description = (TextView) findViewById(R.id.description);
-
-        restaurant.setText(myRestaurants.getRestaurant());
-        description.setText(myRestaurants.getDescription()); }
-
-
+        Random randomGenerator = new Random();
         ArrayList<Restaurants> newArray = new ArrayList<>();
         int arraySize = Restaurants.breakfastPlaces.length;
 
+        // checks each object in breakfastPlaces array if they match option to add to new array
+
         for (int i = 0; i <= arraySize; i++) {
-            if (MY_SERVICE_TYPE.equalsIgnoreCase("fast food")) {
-                
+            if (MY_PRICE_PREFERENCE.equalsIgnoreCase("low")) {
+                if (Restaurants.breakfastPlaces[i].getPrice() == 0) {
+                    checkType(newArray, arraySize);
+                }
+            }
+            if (MY_PRICE_PREFERENCE.equalsIgnoreCase("high")) {
+                if (Restaurants.breakfastPlaces[i].getPrice() == 1) {
+                    checkType(newArray, arraySize);
+                }
+            }
+            if (MY_PRICE_PREFERENCE.equalsIgnoreCase("no preference")) {
+                checkType(newArray, arraySize);
             }
         }
 
+        int index = randomGenerator.nextInt(newArray.size());
+        Restaurants chosenRestaurant = Restaurants.breakfastPlaces[index];
+        restaurant.setText(chosenRestaurant.getRestaurant());
+        description.setText(chosenRestaurant.getDescription());
 
     }
+
+    public void checkType(ArrayList newArray, int arraySize) {
+        for (int i = 0; i <= arraySize; i++) {
+            if (MY_SERVICE_TYPE.equalsIgnoreCase("chain")) {
+                if (Restaurants.breakfastPlaces[i].getType().equalsIgnoreCase("chain")) {
+                    newArray.add(Restaurants.breakfastPlaces[i]);
+                }
+            }
+            if (MY_SERVICE_TYPE.equalsIgnoreCase("local")) {
+                if (Restaurants.breakfastPlaces[i].getType().equalsIgnoreCase("local")) {
+                    newArray.add(Restaurants.breakfastPlaces[i]);
+                }
+            }
+            if (MY_SERVICE_TYPE.equalsIgnoreCase("no preference")) {
+                newArray.add(Restaurants.breakfastPlaces[i]);
+            }
+        }
+    }
+
 }
