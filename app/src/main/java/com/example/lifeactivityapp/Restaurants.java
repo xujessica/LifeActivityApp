5c6d0f7f5f67a0eaf443b2506d9beaa50feccbd0
jakelee <jaketl2002@gmail.com>
2019-10-25 01:32:02 -0500

    Jake

--- app/src/main/java/com/example/lifeactivityapp/Restaurants.java
+++ app/src/main/java/com/example/lifeactivityapp/Restaurants.java
@@ -20,7 +20,7 @@ public class Restaurants {
     }
 
     public static final Restaurants[] breakfastPlaces = {
-            new Restaurants("Einstein Bros. Bagels", "American bagel and coffee chain", 0, "chain", 2.0),
+            new Restaurants("Einstein Bros. Bagels", "American bagel and coffee chain ", 0, "chain", 2.0),
             new Restaurants("Dunkin Donuts", "American multinational coffee/donut company and quick service restaurant",0, "chain",2.0),
             new Restaurants("Mcdonalds", "Fast food, limited service restaurant",0, "chain",2.0),
             new Restaurants("Wildberry Pancakes & Cafe", "Bustling, family-oriented eatery features American breakfasts & lunches in a smart setting", 1, "chain",4.5),
